0dc0abe14ed74b2fbb71393ccfb463c5b3fb97d3
Anatoly Pashin <anatoly.pashin@gmail.com>
2014-11-19 18:35:15 +1100

    create tickets on mailgun post to email/post

---
+++ apps/frontend/modules/email/actions/actions.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * email actions.
+ *
+ * @package    helpdesk
+ * @subpackage email
+ * @author     Anatoly Pashin
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class emailActions extends sfActions
+{
+  public function executePost(sfWebRequest $request)
+  {
+    $from = Doctrine_Query::create()
+      ->from('sfGuardUser u')
+      ->addWhere('u.email_address = ?', $request->getParameter('sender'))
+      ->fetchOne()
+    ;
+
+    $ticket = Ticket::createFromArray([
+      'category_id' => 10
+      , 'name' => $request->getParameter('subject')
+      , 'description' =>
+        ($from ? '' : '<div class="alert alert-info">Письмо пришло от ' . htmlspecialchars($request->getParameter('from')) . '</div>')
+        . '<pre>' . $request->getParameter('body-plain') . '</pre>'
+      , 'created_by' => $from ? $from->getId() : 82
+    ]);
+
+    $ticket->save();
+    die;
+  }
+}
