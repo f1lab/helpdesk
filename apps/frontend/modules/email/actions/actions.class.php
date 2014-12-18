f1e984e2d1b3c3560c2da02f28b500b96239ed98
Anatoly Pashin <anatoly.pashin@gmail.com>
2014-12-18 15:38:03 +1100

    feat(ticket from email): add email attachments to created ticket

--- apps/frontend/modules/email/actions/actions.class.php
+++ apps/frontend/modules/email/actions/actions.class.php
@@ -33,6 +33,26 @@ class emailActions extends sfActions
     }
 
     $ticket->save();
+
+    if (count($_FILES)) {
+      $destination = sfConfig::get('sf_upload_dir') . DIRECTORY_SEPARATOR . 'comment-attachments' . DIRECTORY_SEPARATOR;
+      foreach ($_FILES as $file) {
+        $nameParts = explode('.', $file['name']);
+        $extension = array_pop($nameParts);
+        $newName = uniqid() . '.' . $extension;
+        if (move_uploaded_file($file['tmp_name'], $destination . $newName)) {
+          $comment = Comment::createFromArray([
+            'ticket_id' => $ticket->getId()
+            , 'text' => 'Добавлен файл из письма: ' . $file['name']
+            , 'attachment' => $newName
+            , 'created_by' => $from ? $from->getId() : 82
+          ]);
+
+          $comment->save();
+        }
+      }
+    }
+
     die;
   }
 }
