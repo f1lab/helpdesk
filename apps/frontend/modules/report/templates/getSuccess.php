442d2383ff0763fed37d3c909a9a44d09b58a314
Anatoly Pashin <anatoly.pashin@gmail.com>
2015-04-24 16:28:42 +1000

    feat(report): add report summary

--- apps/frontend/modules/report/templates/getSuccess.php
+++ apps/frontend/modules/report/templates/getSuccess.php
@@ -6,7 +6,7 @@
 
 <?php include_partial('form', ['form' => $form]) ?>
 
-<?php if (count($tickets)): ?>
+<?php if (count($tickets)): $remotelyClosed = 0; $deadlineForResponsibleOk = 0; $deadlineForApproveOk = 0; $deadlineOk = 0; ?>
   <table class="table table-condensed table-bordered table-hover">
     <thead>
       <tr>
@@ -25,9 +25,24 @@
     </thead>
     <tbody><?php foreach ($tickets as $ticket):
       $closer = $ticket->getIsClosed() ? ($ticket->getCloser() ?: null) : null;
-      $applier = $ticket->getApplier();
-      // $reglamented = $ticket->getDeadline() ? (strtotime($ticket->getDeadline()) - strtotime($ticket->getCreatedAt())) : null;
       $firstResponsibleRef = $ticket->getFirstResponsibleRef();
+      $applier = $ticket->getApplier();
+
+      if ($ticket->getIsClosed() and $ticket->getIsClosedRemotely()) {
+        $remotelyClosed += 1;
+      }
+
+      if ($ticket->getToCompany()->getDeadlineForSettingResponsible() === 0 or (strtotime($firstResponsibleRef->getCreatedAt()) - strtotime($ticket->getCreatedAt()) < $ticket->getToCompany()->getDeadlineForSettingResponsible())) {
+        $deadlineForResponsibleOk += 1;
+      }
+
+      if ($ticket->getToCompany()->getDeadlineForApproving() === 0 or (strtotime($applier->getCreatedAt()) - strtotime($ticket->getCreatedAt()) < $ticket->getToCompany()->getDeadlineForApproving())) {
+        $deadlineForApproveOk += 1;
+      }
+
+      if ($ticket->getDeadline() === null or (strtotime($ticket->getDeadline()) - strtotime($ticket->getCreatedAt()) > 0)) {
+        $deadlineOk += 1;
+      }
     ?>
       <tr class="<?php //echo (!$ticket->getDeadline() or $worked < $reglamented) ? 'success' : 'error' ?>">
         <td><a href="<?php echo url_for('tickets/show?id='.$ticket->getId()) ?>"><?php echo $ticket->getId() ?></a></td>
@@ -44,6 +59,17 @@
       </tr>
     <?php endforeach; ?></tbody>
   </table>
+
+  <div class="alert alert-info">
+    <ul class="unstyled">
+      <li>Всего заявок за указанный период: <?php $ticketsCount = count($tickets); echo $ticketsCount; ?></li>
+      <li>Заявки, закрытые удалённо: <?php echo $remotelyClosed; ?> (<?php echo round($remotelyClosed / $ticketsCount * 100, 2) ?>%)</li>
+      <li>Вовремя обработанные заявки: <?php echo $deadlineForResponsibleOk; ?> (<?php echo round($deadlineForResponsibleOk / $ticketsCount * 100, 2) ?>%)</li>
+
+      <li>Вовремя принятые в работу заявки: <?php echo $deadlineForApproveOk; ?> (<?php echo round($deadlineForApproveOk / $ticketsCount * 100, 2) ?>%)</li>
+      <li>Закрытые в указанный срок заявки: <?php echo $deadlineOk; ?> (<?php echo round($deadlineOk / $ticketsCount * 100, 2) ?>%)</li>
+    </ul>
+  </div>
 
 <?php else: ?>
   <div class="alert alert-info">Ничего не найдено.</div>
