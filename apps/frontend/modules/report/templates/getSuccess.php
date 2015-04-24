a44263d880b69769b5b983dbb7aebc0ab74b1fbb
Anatoly Pashin <anatoly.pashin@gmail.com>
2015-04-24 15:23:10 +1000

    feat(report): change columns order and contents

--- apps/frontend/modules/report/templates/getSuccess.php
+++ apps/frontend/modules/report/templates/getSuccess.php
@@ -14,46 +14,33 @@
         <th>Автор</th>
         <th>Тема</th>
         <th>Категория</th>
-        <th>Время создания</th>
-        <th>Deadline</th>
-        <th>Закрыта?</th>
+        <th>Дата поступления</th>
 
-        <th>Общее время выполнения</th>
 
-        <th>Регламентированные сроки</th>
-
+        <th>Дата назначения ответственного</th>
+        <th>Дата принятия в работу</th>
+        <th>Дата закрытия</th>
+        <th>Кто закрыл</th>
       </tr>
     </thead>
     <tbody><?php foreach ($tickets as $ticket):
       $closer = $ticket->getIsClosed() ? ($ticket->getCloser() ?: null) : null;
       $applier = $ticket->getApplier();
-      $worked = strtotime($closer ? $closer->getCreatedAt() : date('d.m.Y H:i:s')) - strtotime($applier ? $applier->getCreatedAt() : $ticket->getCreatedAt());
-      $reglamented = $ticket->getDeadline() ? (strtotime($ticket->getDeadline()) - strtotime($ticket->getCreatedAt())) : null;
+      // $reglamented = $ticket->getDeadline() ? (strtotime($ticket->getDeadline()) - strtotime($ticket->getCreatedAt())) : null;
+      $firstResponsibleRef = $ticket->getFirstResponsibleRef();
     ?>
-      <tr class="<?php echo (!$ticket->getDeadline() or $worked < $reglamented) ? 'success' : 'error' ?>">
+      <tr class="<?php //echo (!$ticket->getDeadline() or $worked < $reglamented) ? 'success' : 'error' ?>">
         <td><a href="<?php echo url_for('tickets/show?id='.$ticket->getId()) ?>"><?php echo $ticket->getId() ?></a></td>
         <td>@<?php echo $ticket->getCreator()->getUsername() ?></td>
         <td><?php echo $ticket->getName() ?></td>
         <td><?php echo $ticket->getCategory() ?></td>
         <td><?php echo date('d.m.Y H:i:s', strtotime($ticket->getCreatedAt())) ?></td>
-        <td><?php echo $ticket->getDeadline() ? date('d.m.Y H:i:s', strtotime($ticket->getDeadline())) : '—' ?></td>
-        <td><?php if ($ticket->getIsClosed()): ?>
-          Заявку закрыл @<?php echo $closer ? $closer->getCreator()->getUsername() : 'неизвестно кто' ?>
-          <?php echo $closer ? date('d.m.Y H:i:s', strtotime($closer->getCreatedAt())) : 'неизвестно когда' ?>
-        <?php else: ?>
-          не закрыта
-        <?php endif ?></td>
 
-        <td>
-          <?php echo Helpdesk::formatDuration($worked) ?>
-        </td>
 
-        <td><?php if ($reglamented): ?>
-          <?php echo Helpdesk::formatDuration($reglamented) ?>
-        <?php else: ?>
-          —
-        <?php endif ?></td>
-
+        <td><?php echo ($firstResponsibleRef && $firstResponsibleRef->getCreatedAt() !== '2015-01-01 00:00:00') ? date('d.m.Y H:i:s', strtotime($firstResponsibleRef->getCreatedAt())) : '—' ?></td>
+        <td><?php echo $applier ? date('d.m.Y H:i:s', strtotime($applier->getCreatedAt())) : '—' ?></td>
+        <td><?php echo $closer ? date('d.m.Y H:i:s', strtotime($closer->getCreatedAt())) : '—' ?></td>
+        <td><?php echo $closer ? $closer->getCreator() : '—' ?></td>
       </tr>
     <?php endforeach; ?></tbody>
   </table>
