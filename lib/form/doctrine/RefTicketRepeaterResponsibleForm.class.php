9171d9cca72126a52f27edd57c0b0482f1db6f1f
Anatoly Pashin <anatoly.pashin@gmail.com>
2015-08-04 17:16:55 +1000

    feat(ticket-repeater): schema, migrations and classes

---
+++ lib/form/doctrine/RefTicketRepeaterResponsibleForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * RefTicketRepeaterResponsible form.
+ *
+ * @package    helpdesk
+ * @subpackage form
+ * @author     Anatoly Pashin
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class RefTicketRepeaterResponsibleForm extends BaseRefTicketRepeaterResponsibleForm
+{
+  public function configure()
+  {
+  }
+}
