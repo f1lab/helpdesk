f1lab/helpdesk

commit 3aac5291812ae2d6054ab43cbefaef13567f594f

feat(comment): add to observers
fixes #7

lib/model/doctrine/Comment.class.php
@@ -121,6 +121,28 @@ class Comment extends BaseComment
         }
       }
     }
+
+    // add to observers
+    if (
+      $this->getCreatedBy() != $this->getTicket()->getCreatedBy()
+      && true == ($user = sfContext::getInstance()->getUser()->getGuardUser())
+    ) {
+      $observingAlready = Doctrine_Query::create()
+        ->from('RefTicketObserver ref')
+        ->addWhere('ref.user_id = ?', $user->getId())
+        ->addWhere('ref.ticket_id = ?', $this->getTicket()->getId())
+        ->count() !== 0
+      ;
+
+      if (!$observingAlready) {
+        // add to observers
+        $observeRecord = RefTicketObserver::createFromArray([
+          'user_id' => $user->getId(),
+          'ticket_id' => $this->getTicket()->getId(),
+        ]);
+        $observeRecord->save();
+      }
+    }
   }
 
   public function getChangedTicketStateToLabel()

commit 541c71451690cec10c932f869ca4bc02bcbf8c3d

fix(comment): don't add to observers if responsilbe
regression from 3aac5291812ae2d6054ab43cbefaef13567f594f

lib/model/doctrine/Comment.class.php
@@ -133,8 +133,14 @@ class Comment extends BaseComment
         ->addWhere('ref.ticket_id = ?', $this->getTicket()->getId())
         ->count() !== 0
       ;
+      $isResponsible = Doctrine_Query::create()
+          ->from('RefTicketResponsible ref')
+          ->addWhere('ref.user_id = ?', $user->getId())
+          ->addWhere('ref.ticket_id = ?', $this->getTicket()->getId())
+          ->count() !== 0
+        ;
 
-      if (!$observingAlready) {
+      if (!$observingAlready and !$isResponsible) {
         // add to observers
         $observeRecord = RefTicketObserver::createFromArray([
           'user_id' => $user->getId(),